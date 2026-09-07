2c768cdf4cce327940359da79aecdc234eacc75c
Neko <modoggu12345@outlook.com>
2026-09-07 16:08:08 +0900

    fix: ミス

--- cogs/down.py
+++ cogs/down.py
@@ -38,7 +38,7 @@ class DownCog(commands.Cog):
     @commands.cooldown(2, 5, type=commands.BucketType.user)
     async def status(self, ctx: commands.Context):
         guild = self.bot.get_guild(1343124570131009579)
-        bots = [guild.get_member(1322100616369147924), guild.get_member(1392853908879179936)]
+        bots = [guild.get_member(1322100616369147924), guild.get_member(1343156909242454038), guild.get_member(1502156877998460959), guild.get_member(1537386704137363527), guild.get_member(1537996178157871154)]
         status_text = "\n".join([f"{STATUS_EMOJIS.get(b.status)} {b.name}" for b in bots])
         await ctx.send(embed=discord.Embed(title="各Botのステータス", description=status_text, color=discord.Color.blue()))
 
